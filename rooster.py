f059a557f58c64e01739cd05dd18795540719078
Micha den Heijer <micha@michadenheijer.com>
2019-03-08 19:39:30 +0100

    updates

--- rooster.py
+++ rooster.py
@@ -1,47 +1,54 @@
-from to_google_cal import add_event, delete_events
-import sys
-import os.path
+"""
+This is the main program
+"""
+import os
 from os import path
+
+import sys
 import threading
 import datetime
 import json
 import webuntis
 
-current = os.path.dirname(sys.argv[0])
-today = datetime.date.today()
-end = today + datetime.timedelta(days=14)
+from to_google_cal import add_event, delete_events
 
-with open(current + "config.json") as f:
-    data = json.load(f)
-    global username, password, server, school, studentID
-    username = data["username"]
-    password = data["password"]
-    server = data["server"]
-    school = data["school"]
-    studentID = data["webuntis_id"]
+
+CURRENT = os.path.dirname(sys.argv[0])
+TODAY = datetime.date.today()
+END = TODAY + datetime.timedelta(days=14)
+
+with open(CURRENT + "config.json") as FILE:
+    DATA = json.load(FILE)
+    global USERNAME, PASSWORD, SERVER, SCHOOL, STUDENTID
+    USERNAME = DATA["username"]
+    PASSWORD = DATA["password"]
+    SERVER = DATA["server"]
+    SCHOOL = DATA["school"]
+    STUDENTID = DATA["webuntis_id"]
 
 try:
-    if path.exists(current + "wrong_pass"):
+    if path.exists(CURRENT + "wrong_pass"):
         print("Last time wrong password, not trying again")
         exit()
     with webuntis.Session(
-        username=username,
-        password=password,
-        server=server,
-        school=school,
+        username=USERNAME,
+        password=PASSWORD,
+        server=SERVER,
+        school=SCHOOL,
         useragent='Chrome'
     ).login() as s:
-        lesson_request = s.timetable(student=studentID, start=today, end=end)
+        LESSON_REQ = s.timetable(student=STUDENTID, start=TODAY, end=END)
         delete_events()
-        for lesson in lesson_request:
+        for lesson in LESSON_REQ:
             if lesson.code != 'cancelled':
                 lesson.rooms = str(lesson.rooms).replace("[", "")
                 lesson.rooms = str(lesson.rooms).replace("]", "")
                 lesson.subjects = str(lesson.subjects).replace("[", "")
                 lesson.subjects = str(lesson.subjects).replace("]", "")
-                thread = threading.Thread(target=add_event, args=(lesson.start, lesson.end, lesson.subjects, lesson.rooms,))
+                thread = threading.Thread(target=add_event, \
+                    args=(lesson.start, lesson.end, lesson.subjects, lesson.rooms,))
                 thread.start()
 except webuntis.errors.BadCredentialsError:
     print("Wrong password")
-    f = open(current + "wrong_pass", "w+")
-    f.close()
+    FILE = open(CURRENT + "wrong_pass", "w+")
+    FILE.close()
