42101ac9b33e5508c59ed1c63967a4f7d82e8c6d
michadenheijer <micha@michadenheijer.com>
2019-10-14 17:48:58 +0200

    Set deps to fixed version, run config.py correctly, combine file and folder correctly, raise WrongPasswordError

--- schedule.py
+++ schedule.py
@@ -5,23 +5,31 @@ import os
 from os import path
 
 import sys
-import threading
 import datetime
 import json
 import subprocess
 import webuntis
 
+import config
+
 CURRENT = os.path.dirname(sys.argv[0])
 TODAY = datetime.date.today()
 END = TODAY + datetime.timedelta(days=14)
+SCHEDULE = os.path.join(CURRENT, "config.json")
+WRONG_PASS = os.path.join(CURRENT, "wrong_pass")
 
-if not path.exists(CURRENT + 'config.json'):
-    CONFIG = ['python3', 'config.py']
-    subprocess.Popen(CONFIG).wait()
+class WrongPasswordError(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+if not path.exists(SCHEDULE):
+    config.config()
 
 from google_cal import add_event, delete_events
 
-with open(CURRENT + "config.json") as FILE:
+with open(SCHEDULE) as FILE:
     DATA = json.load(FILE)
     global USERNAME, PASSWORD, SERVER, SCHOOL, STUDENTID
     USERNAME = DATA["username"]
@@ -31,9 +39,8 @@ with open(CURRENT + "config.json") as FILE:
     STUDENTID = DATA["webuntis_id"]
 
 try:
-    if path.exists(CURRENT + "wrong_pass"):
-        print("Last time wrong password, not trying again")
-        exit()
+    if path.exists(WRONG_PASS):
+        raise WrongPasswordError("Before you've entered a wrong password, run config.py to fix the error.")
     with webuntis.Session(
         username=USERNAME,
         password=PASSWORD,
@@ -49,10 +56,8 @@ try:
                 lesson.rooms = str(lesson.rooms).replace("]", "")
                 lesson.subjects = str(lesson.subjects).replace("[", "")
                 lesson.subjects = str(lesson.subjects).replace("]", "")
-                thread = threading.Thread(target=add_event, \
-                    args=(lesson.start, lesson.end, lesson.subjects, lesson.rooms,))
-                thread.start()
+                add_event(lesson.start, lesson.end, lesson.subjects, lesson.rooms)
 except webuntis.errors.BadCredentialsError:
-    print("Wrong password")
-    FILE = open(CURRENT + "wrong_pass", "w+")
+    FILE = open(WRONG_PASS, "w+")
     FILE.close()
+    raise WrongPasswordError("The password you entered appears to be wrong.")
